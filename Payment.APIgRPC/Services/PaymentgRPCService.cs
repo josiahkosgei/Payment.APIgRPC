3dd72cfe14b6eac2d5ce2bb01e7e8d1cc42aac28
Josiah Kosgei <jtomno90@gmail.com>
2020-09-09 16:56:29 +0300

    Add project files.

---
+++ Payment.APIgRPC/Services/PaymentgRPCService.cs
@@ -0,0 +1,49 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+using PaymentService;
+
+namespace Payment.APIgRPC.Services
+{
+    public class PaymentgRPCService : PaymentService.PaymentService.PaymentServiceBase
+    {
+        private readonly ILogger<PaymentgRPCService> _logger;
+        public PaymentgRPCService(ILogger<PaymentgRPCService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<MakePaymentReply> MakePayment(MakePaymentRequest request, ServerCallContext context)
+        {
+            var transactionId = Guid.NewGuid().ToString();
+
+            _logger.LogInformation("---------------------------------------");
+            _logger.LogInformation($"Make payment {transactionId}");
+            _logger.LogInformation("---------------------------------------");
+
+            return Task.FromResult(new MakePaymentReply
+            {
+                TrackingId = transactionId
+            });
+        }
+        // Mock Get Payment Details
+        public override Task<GetPaymentDetailsReply> GetPaymentDetails(GetPaymentDetailsRequest request, ServerCallContext context)
+        {
+            var trackingId = Guid.NewGuid().ToString();
+            _logger.LogInformation("---------------------------------------");
+            _logger.LogInformation($"Get Payment Details {trackingId}");
+            _logger.LogInformation("---------------------------------------");
+
+            return Task.FromResult(new GetPaymentDetailsReply
+            {
+                TrackingId = trackingId,
+                PaymentMethod="VISA",
+                Reference= Guid.NewGuid().ToString(),
+                PaymentStatus="COMPLETED"
+                
+            });
+        }
+    }
+}
---
+++ Payment.APIgRPC_Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+using PaymentService;
+using Grpc.Core;
+
+namespace Payment.APIgRPC_Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var paymentClient = new PaymentService.PaymentService.PaymentServiceClient(channel);
+
+            Console.WriteLine("Welcome to the gRPC client");
+
+            var reply = await paymentClient.MakePaymentAsync(new MakePaymentRequest
+            {
+               Amount="2.00",
+               TrackingId = Guid.NewGuid().ToString(),
+               BuyerName = "John Doe",
+               Email = "f.doe@example.com",
+               PaymentMethod = "VISA",
+               PaymentStatus = "COMPLETED",
+               PhoneNumber = "422 5451 332",
+               Reference = Guid.NewGuid().ToString(),
+            });
+
+            Console.WriteLine($"Made payment: {reply.TrackingId}");
+
+            using var statusReplies = paymentClient.GetPaymentDetails(new GetPaymentDetailsRequest{ TrackingId = reply.TrackingId, Reference=reply.Reference });
+            
+            while (await statusReplies.ResponseStream.MoveNext())
+            {
+                var statusReply = statusReplies.ResponseStream.Current.PaymentMethod;
+                Console.WriteLine($"Payment status: {statusReply}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
